452fdbdc59e172b1323f2067f5082dd5142b9ed2
Septian Maulana <septianm028@gmail.com>
2023-03-07 15:08:37 +0700

    build: first init boilerplate project

---
+++ index.js
@@ -0,0 +1,25 @@
+// render function
+
+const DUMMY_DATA = [
+  {
+    title: "MANCHESTER PAYAH BANGET",
+    author: "fachry",
+    score: 0,
+  },
+  {
+    title: "Manchester Payah Banget",
+    author: "septian",
+    score: 0,
+  },
+  {
+    title: "MANCHESTER PAYAH BANGET!!!",
+    author: "fahri",
+    score: 0,
+  },
+];
+
+function render() {
+  console.log(DUMMY_DATA);
+}
+
+render();
